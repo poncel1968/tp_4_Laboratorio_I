ae26d41d89851791400721862e20c5ef5747ee42
alumno <alumno@LAB00PC00>
2018-11-13 19:17:59 -0300

    1er

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -553,7 +553,6 @@ int ll_sort(LinkedList* this, int (*pFunc)(void* ,void*), int order)
 {
     int returnAux =-1;
     int j;
-    Node * auxNode=NULL;
     Node * auxNode1;
     Node * auxNode2;
     void * auxElement1;
@@ -600,4 +599,48 @@ int ll_sort(LinkedList* this, int (*pFunc)(void* ,void*), int order)
 
     return returnAux;
 
+}
+
+LinkedList* ll_filter(LinkedList* this, int (*pFunc)(void*))
+{
+    LinkedList* retorno = NULL;
+    int i;
+    LinkedList* listfilter;
+    void * pElement=NULL;
+
+    if(this != NULL && pFunc!=NULL)
+    {
+        listfilter=ll_newLinkedList();
+        for (i=0; i<ll_len(this); i++)
+        {
+            pElement=ll_get(this,i);
+            if(pFunc(pElement)==0)
+            {
+                ll_add(listfilter,pElement);
+            }
+        }
+        retorno = listfilter;
+    }
+    return retorno;
+}
+
+
+
+int ll_map(LinkedList* this, int (*pFunc)(void*))
+{
+    int retorno=-1;
+    int i;
+    void * pElement=NULL;
+
+    if(this !=NULL && pFunc !=NULL)
+    {
+        for (i=0;i<ll_len(this);i++)
+        {
+            pElement= ll_get(this,i);
+            pFunc(pElement);
+        }
+        retorno=0;
+    }
+
+    return retorno;
 }
